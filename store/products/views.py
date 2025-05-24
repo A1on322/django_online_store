f315f301da24d2d65139ce6a44dd9074d3bf1792
A1on322 <alexandr.lee322>
2025-05-24 12:01:27 +0900

    Created app products

---
+++ store/products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+
+# Create your views here.
+
+class ProductHome(ListView):
+    template_name = "products/index.html"
+    context_object_name = "products"
+    title_page = "Home"
+
+    def get_queryset(self):
+        return {'data':' Hello '}
