3a54035200932f67da2797122efc24838fbd13b0
A1on322 <alexandr.lee322>
2025-05-26 14:06:10 +0900

    set django admin up

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from products.models import Product, ProductCategory
+
 
 # Register your models here.
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'quantity', 'category', 'post_image']
+    list_filter = ['category__name']
+
+
+    @admin.display(description="Image", ordering='content')
+    def post_image(self, product: Product):
+        if product.image:
+            return mark_safe(f"<img src='{product.image.url}' width=50>")
+        return "No image"
--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Product Categories"
+        verbose_name = "Product Category"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        verbose_name = "Products"
+
+    def __str__(self):
+        return self.name
+
