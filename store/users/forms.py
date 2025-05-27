fa6231c3c9e57dfdd28e2ff39f7b0e97e2722c9a
A1on322 <alexandr.lee322>
2025-05-27 20:19:47 +0900

    added login from

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,9 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 
 
+
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label="Username",
-                               widget=forms.TextInput())
+                               widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
     password = forms.CharField(label="Password",
-                               widget=forms.PasswordInput())
+                               widget=forms.PasswordInput(attrs={'class': 'form-control py-4'}))
+
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password')
