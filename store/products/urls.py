f4409a028c0fe6374af84d2998d1c703a8ec71c5
A1on322 <alexandr.lee322@gmail.com>
2025-05-28 20:58:24 +0900

    added routes for adding and deleting items from cart

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from products import views
+from products.views import cart_add, cart_remove
 
 app_name = 'products'
 
 urlpatterns = [
     path("", views.ProductsView.as_view(), name="index"),
+    path('cart/add/<int:product_id>/', cart_add, name='cart_add' ),
+    path('cart/remove/<int:cart_id>/', cart_remove, name='cart_remove'),
+
+
 ]
