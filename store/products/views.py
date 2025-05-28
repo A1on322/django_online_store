10dc3e38eb8f1516e795b6d9d1d816e7a14eae85
A1on322 <alexandr.lee322@gmail.com>
2025-05-28 20:58:54 +0900

    added controllers for removing and adding items in cart

--- store/products/views.py
+++ store/products/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from products.models import Product
+from products.models import Product, Cart
 
 
 # Create your views here.
@@ -22,3 +23,21 @@ class ProductsView(ListView):
 
     def get_queryset(self):
         return Product.objects.all()
+
+def cart_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart = Cart.objects.filter(user_id=request.user.id, product = product)
+
+    if not cart.exists():
+        Cart.objects.create(user_id=request.user.id, product = product, quantity=1)
+    else:
+        c = cart.first()
+        c.quantity += 1
+        c.save()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
