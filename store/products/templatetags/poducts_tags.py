fff72adf689bcd34f6709b206ea23cba9ed3ae96
A1on322 <alexandr.lee322@gmail.com>
2025-05-29 13:49:01 +0900

    Edited template inclusion tag

--- store/products/templatetags/poducts_tags.py
+++ store/products/templatetags/poducts_tags.py
@@ -4,7 +4,10 @@ from products.models import ProductCategory
 
 register = template.Library()
 
-@register.inclusion_tag('products/list_cats.html')
-def show_categories():
+@register.inclusion_tag('products/list_cats.html', takes_context=True)
+def show_categories(context):
     cats = ProductCategory.objects.all()
-    return {'cats': cats}
+    return {
+        'cats': cats,
+        'cat_selected': context.get('cat_selected', None),
+    }
