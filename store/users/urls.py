d553a3a5fe60c25238f4782e9e280ee9341794ec
A1on322 <alexandr.lee322@gmail.com>
2025-05-30 15:54:28 +0900

    Added routes for password reset and password change views

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path, reverse_lazy
-from .views import LoginUserView, RegisterUserView, ProfileUserView
+from .views import LoginUserView, RegisterUserView, ProfileUserView, CustomPasswordResetView, \
+    CustomPasswordResetConfirmView
 
 app_name = 'users'
 
@@ -9,4 +10,8 @@ urlpatterns = [
     path("register/", RegisterUserView.as_view(), name="register"),
     path('profile/', ProfileUserView.as_view(), name='profile'),
     path('logout/', LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+
 ]
