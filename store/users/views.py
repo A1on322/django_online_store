d7769f534c9dfc21bd2492e6d995df90de57a382
A1on322 <alexandr.lee322@gmail.com>
2025-05-30 15:55:14 +0900

    Created custom PasswordReset and PasswordResetConfirm views

--- store/users/views.py
+++ store/users/views.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import get_user, get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
 from products.models import Cart
-from users.forms import LoginUserForm, RegisterUserForm, ProfileUserForm
+from users.forms import LoginUserForm, RegisterUserForm, ProfileUserForm, CustomPasswordResetForm, \
+     CustomSetPasswordForm
 
 
 # Create your views here.
@@ -21,25 +23,17 @@ class LoginUserView(LoginView):
         return reverse_lazy('home')
 
 
-class RegisterUserView(CreateView):
+class RegisterUserView(SuccessMessageMixin, CreateView):
     form_class = RegisterUserForm
     template_name = 'users/register.html'
     extra_context = {'title': 'Registration'}
     success_url = reverse_lazy('users:login')
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, "You have successfully registered! Please log in.")
-        return response
+    success_message = 'You have successfully registered! Please log in.'
 
 class ProfileUserView(LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = ProfileUserForm
     template_name = 'users/profile.html'
-    extra_context = {'title': 'Profile',
-                     'default_image': settings.DEFAULT_USER_IMAGE
-                     }
-
 
     def get_success_url(self):
         return reverse_lazy('users:profile')
@@ -50,4 +44,23 @@ class ProfileUserView(LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cart'] = Cart.objects.filter(user=self.request.user)
+        context['title'] = 'Profile'
+        context['default_image'] = settings.DEFAULT_USER_IMAGE
         return context
+
+
+class CustomPasswordResetView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset_form.html'
+    email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset')
+    success_message = ('We’ve emailed you instructions for setting your password, if an account exists with the email'
+                        ' you entered.\nYou should receive them shortly.')
+    form_class = CustomPasswordResetForm
+
+
+class CustomPasswordResetConfirmView(SuccessMessageMixin, PasswordResetConfirmView):
+    template_name = 'users/password_reset_confirm.html'
+    success_url = reverse_lazy('users:login')
+    success_message = 'You have successfully reset your password!\nPlease log in.'
+    form_class = CustomSetPasswordForm
+
