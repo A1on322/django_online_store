428a685928773a71e0b6cbd55d003a602a5afe2b
A1on322 <alexandr.lee322@gmail.com>
2025-06-08 23:58:10 +0900

    Added orders app

--- store/store/settings.py
+++ store/store/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'users',
+    'orders',
     'social_django',
     'debug_toolbar',
 ]
@@ -86,6 +87,15 @@ INTERNAL_IPS = [
     'localhost',
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
