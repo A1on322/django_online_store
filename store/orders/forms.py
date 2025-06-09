6eb79ac769ee376ef6ae6d94f4ff4dfbd5ba0c9b
A1on322 <alexandr.lee322@gmail.com>
2025-06-09 19:55:27 +0900

    Created OrderForm

--- store/orders/forms.py
+++ store/orders/forms.py
@@ -4,6 +4,20 @@ from orders.models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'you@example.com'}),
+    )
+    address = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+    )
+
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address',]
