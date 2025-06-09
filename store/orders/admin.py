d8e344fdda7e9bbb86710b767c660ef10bf99c68
A1on322 <alexandr.lee322@gmail.com>
2025-06-09 19:54:28 +0900

    Registered Order model into admin panel

--- store/orders/admin.py
+++ store/orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from orders.models import Order
+
+
 # Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'status')
+    fields = (
+        'id',
+        ('first_name', 'last_name'),
+        ('email', 'address'),
+        'cart',
+        'created_at',
+        'status',
+        'user'
+    )
+    readonly_fields = ('id', 'created_at')
