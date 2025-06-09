a086ea1e96dfef3c1cd7090c92b211cb43304c78
A1on322 <alexandr.lee322@gmail.com>
2025-06-09 19:55:56 +0900

    Added context processor

--- store/store/settings.py
+++ store/store/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
+                'products.context_processors.cart',
             ],
         },
     },
