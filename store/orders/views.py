52141a7c2f4de72b7041b12e77e445bbfa184ab5
A1on322 <alexandr.lee322@gmail.com>
2025-06-09 19:56:26 +0900

    View for OrderCreate

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from orders.forms import OrderCreateForm
@@ -6,4 +7,9 @@ from orders.forms import OrderCreateForm
 class OrderCreateView(CreateView):
     template_name = 'orders/orders_create.html'
     form_class = OrderCreateForm
+    success_url = reverse_lazy('orders:order_create')
+    extra_context = {'title': 'Create new order'}
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
