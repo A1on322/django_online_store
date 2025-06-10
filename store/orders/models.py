c9aa4f9769350db7d9485fde6ad92745d9742c9c
A1on322 <alexandr.lee322@gmail.com>
2025-06-10 22:47:37 +0900

    Added updating order model after successful payment

--- store/orders/models.py
+++ store/orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from users.models import User
+from products.models import Cart
 
 
 class Order(models.Model):
@@ -26,5 +27,15 @@ class Order(models.Model):
 
     def __str__(self):
         return f'Order #{self.id}: {self.first_name} {self.last_name}'
+
+    def update_after_payment(self):
+        cart = Cart.objects.filter(user=self.user)
+        self.status = self.PAID
+        self.cart = {
+            'purchased_items': [item.get_json() for item in cart],
+            'total': float(cart.get_total_price()),
+        }
+        cart.delete()
+        self.save()
 
 
